b71a249d0bfa2e1d3cd50dda8e84b48479c035b4
wajdAlmansor <wajd2828@gmail.com>
2025-07-26 18:47:01 +0300

    Initial Commit

---
+++ WALLEAP/WALLEAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WALLEAPApp.swift
+//  WALLEAP
+//
+//  Created by Wajd on 26/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct WALLEAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
